dada6c45cf17f7e490049d6893cce08ec0568cdc
Christie <Christie.Desnoyer@gmail.com>
2018-09-26 19:30:27 -0400

    second commit for Lab 2

--- src/main.java
+++ src/main.java
@@ -1,41 +1,68 @@
-	import java.text.SimpleDateFormat;
-	import java.util.Date;
-	import java.util.Scanner;
-	import java.util.concurrent.TimeUnit;
 
-	class Main
-	{
-	    public static void main (String[] args) throws java.lang.Exception
-	    {
-	    	double dateYears;
-	    	
-	    	
-	        
-	        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.println("Hello!");
-	        System.out.println("This program will help calculate");
-	        System.out.println("the amount of days between two dates.");
-	        System.out.println("");
-	        System.out.println("Please enter first date in MM/dd/YYYY format.");
-	        System.out.println("Example; August 12th, 1988 would be 08/12/1988.");
-	        Date firstDate= format.parse(scanner.nextLine());
-	        System.out.println("Please enter second date in MM/dd/YYYY format.");
-	        Date secondDate= format.parse(scanner.nextLine());
-	        scanner.close();
-	        
-	       
 
-	        
-	        long diff = firstDate.getTime()-secondDate.getTime();
-	        long diffDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-	        diffDays = Math.abs(diffDays);
-	        dateYears = (diffDays / 365);
-	        System.out.println(diffDays + ", is the number of days between dates.");
-	        System.out.println(dateYears + ", is the number of years between each dates." );
-	        System.out.println("Thank you for using this program!");
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+
+public class main {
+	public static void main(String args[]) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+	    //Program greets user 
+		System.out.println("Hello!");
+		
+		//explanation of what this program will do for the user
+        System.out.println("This program will help calculate"); 
+        System.out.println("the amount of days between two dates.");
+        System.out.println("");
+        System.out.println("Please enter first date in MM/dd/YYYY format."); 
+        
+        //Prompt user to enter first date
+        System.out.println("Example; August 12th, 1988 would be 08/12/1988."); 
+        
+        //Scanner for the first date
+		String firstDate = scanner.nextLine();
+		//formats date into months, days, years
+		DateTimeFormatter formatter1=DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		//Convert the string data to LocalDate
+	    LocalDate localOne = LocalDate.parse(firstDate, formatter1);
+		
+	    //prompt user for second date
+		System.out.println("Please enter second date in MM/dd/YYYY format."); 
+		//scan for secondDate
+		String secondDate = scanner.nextLine();
+		//formats date into Months, days, years
+		DateTimeFormatter formatter2=DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		//Convert string data into the LocalDate 
+	    LocalDate localTwo = LocalDate.parse(secondDate, formatter2);
+        scanner.close();
 	
 
-	    }
+        
+        Period intervalPeriod = Period.between(localOne, localTwo);
+        
+        System.out.print("There are " + intervalPeriod.getYears());
+        System.out.print(" years, " + intervalPeriod.getMonths() +" months, ");
+        System.out.println("and " + intervalPeriod.getDays() + " days, between the two dates.");
+        System.out.println("Thank you for using this program, and have a great day!");
+		
+       }
+       
 	}
+
+	
+	
+		
+		
+	
+
+		
+		
+	
+		
+	
 
